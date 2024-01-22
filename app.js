f74bb747f7bc15b94c96e13f442ea0e70be504cf
Yesid <yesidrodriguez305@gmail.com>
2024-01-22 16:29:30 -0500

    Add a POST route to create a new movie

--- app.js
+++ app.js
@@ -3,9 +3,12 @@
 // Sus principios son: escabilidad, simplicidad, portabilidad, visibilidad, fiabilidad y modificabilidad
 
 const express = require('express') // npm install express -E para que se instale la versión exacta que se tiene en package.json
+const crypto = require('node:crypto') // node:crypto es un módulo nativo de node.js para encriptar datos
 const movies = require('./movies.json')
+const { validateMovie } = require('./schemas/movies')
 
 const app = express()
+app.use(express.json())
 app.disable('x-powered-by')
 
 const PORT = process.env.PORT ?? 3000
@@ -30,6 +33,32 @@ app.get('/movies/:id', (req, res) => { // :id es un parámetro de la ruta -> de 
   if (movie) return res.json(movie)
 
   res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+  // para validacion de datos, zod.dev -> npm install zod -E
+
+  if (result.error) {
+    // tambien se puede usar 422 Unprocessable Entity
+    return res.status(400).json({ errors: JSON.parse(result.error.message) })
+  }
+
+  const newMovie = {
+    id: crypto.randomUUID(), // genera un id aleatorio con la libreria crypto
+    // ...req.body // es mala practica porque se puede enviar cualquier cosa en el body si no se tiene un schema
+    ...result.data
+    // title,
+    // genre,
+    // director,
+    // year,
+    // duration,
+    // rate: rate ?? 0, // si no se envia el rate, se le asigna 0
+    // poster
+  }
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
 })
 
 app.listen(PORT, () => {
